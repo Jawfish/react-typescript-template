5204ea66c6d9c906dd1b15cd10ec0684c787c89d
James Fitzgerald <kaphix@gmail.com>
2022-09-20 11:39:27 -0400

    update configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,99 @@
 /* eslint-disable no-undef */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended'
+  ],
   parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+    'import',
+    'tailwindcss',
+    'simple-import-sort',
+    'promise'
+  ],
   root: true,
-	env: {
-		browser: true,
-		es2022: true,
-	}
+  env: {
+    browser: true,
+    es2022: true
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json'
+      }
+    }
+  },
+  rules: {
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'promise/prefer-await-to-then': 'error',
+    'promise/prefer-await-to-callbacks': 'error',
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        bracketSameLine: true,
+        bracketSpacing: true,
+        embeddedLanguageFormatting: 'auto',
+        endOfLine: 'lf',
+        htmlWhitespaceSensitivity: 'css',
+        insertPragma: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: 'preserve',
+        quoteProps: 'as-needed',
+        requirePragma: false,
+        semi: true,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'none',
+        useTabs: false
+      }
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'object',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index'
+        ],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after'
+          }
+        ],
+        'newlines-between': 'always'
+      }
+    ]
+  }
 };
