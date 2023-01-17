47093bd9ce762057108d8a8e24fffe39e07abe0b
James Fitzgerald <Kaphix@gmail.com>
2023-01-16 22:30:31 -0500

    style: run eslint on files outside of src

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-disable no-undef */
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [],
-	darkMode: 'class',
+	darkMode: 'class'
 };
-
